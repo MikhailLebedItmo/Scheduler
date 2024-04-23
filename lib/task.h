8fdeb15caa7e25e9a3c57c544adf5d1ac48120d7
mike <408995@edu.itmo.ru>
2024-04-23 05:11:14 +0300

    create task class

--- lib/task.h
+++ lib/task.h
@@ -1,8 +1,39 @@
-//
-// Created by mikha on 4/23/2024.
-//
+#pragma once
+#include "any.h"
 
-#ifndef TASK_SCHEDULER_TASK_H
-#define TASK_SCHEDULER_TASK_H
 
-#endif //TASK_SCHEDULER_TASK_H
+class BaseTask {
+public:
+    virtual void execute() = 0;
+
+    virtual Any& get_result() = 0;
+
+    virtual ~BaseTask() = 0;
+};
+
+template <typename Func, typename FirstArg, typename SecondArg>
+class Task: BaseTask {
+public:
+    void execute() override {
+        if (is_completed()) {
+            return;
+        }
+        result = func(std::move(first_arg), std::move(second_arg));
+    }
+
+    Any& get_result() override {
+        execute();
+        return result;
+    }
+
+    bool is_completed() const {
+        return !result.empty();
+    }
+private:
+    FirstArg first_arg;
+    SecondArg second_arg;
+    Any result;
+    [[no_unique_address]] Func func;
+};
+
+
